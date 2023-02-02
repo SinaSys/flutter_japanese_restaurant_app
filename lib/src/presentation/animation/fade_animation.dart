f55bbe613a635083af1b1311e86b238ac82502be
sina <sinasystem47@gmail.com>
2023-02-02 19:05:00 +0330

    Cubit : Migrate simple_animations package from version 4 to version 5

--- lib/src/presentation/animation/fade_animation.dart
+++ lib/src/presentation/animation/fade_animation.dart
@@ -7,30 +7,33 @@ class FadeAnimation extends StatelessWidget {
   final double delay;
   final Widget child;
 
-  const FadeAnimation({Key? key, required this.delay, required this.child})
-      : super(key: key);
+  const FadeAnimation({
+    Key? key,
+    required this.delay,
+    required this.child,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final tween = MultiTween<AniProps>()
-      ..add(
+    final tween = MovieTween()
+      ..tween(
         AniProps.opacity,
         Tween<double>(begin: 0.0, end: 1.0),
-        const Duration(milliseconds: 500),
+        duration: const Duration(milliseconds: 500),
       )
-      ..add(
+      ..tween(
         AniProps.translateY,
         Tween<double>(begin: -30.0, end: 0.0),
-        const Duration(milliseconds: 500),
-        Curves.easeOut,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOut,
       );
 
-    return PlayAnimation<MultiTweenValues<AniProps>>(
+    return PlayAnimationBuilder<Movie>(
       delay: Duration(milliseconds: (500 * delay).round()),
       duration: tween.duration,
       tween: tween,
       child: child,
-      builder: (context, child, value) => Opacity(
+      builder: (_, Movie value, Widget? child) => Opacity(
         opacity: value.get(AniProps.opacity),
         child: Transform.translate(
           offset: Offset(0, value.get(AniProps.translateY)),
