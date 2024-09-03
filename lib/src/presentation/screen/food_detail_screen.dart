ea9f7a1cbcdeb525464bfc82c33769d25d386abd
RotRot-pi <ramyofficel@gmail.com>
2024-09-03 21:35:25 +0100

    Fix the BottomAppBar not appearing when entering Food Detail Screen

--- lib/src/presentation/screen/food_detail_screen.dart
+++ lib/src/presentation/screen/food_detail_screen.dart
@@ -66,119 +66,136 @@ class FoodDetailScreen extends StatelessWidget {
             .add(FavoriteItemEvent(foodList[foodList.getIndex(food)])),
       ),
       bottomNavigationBar: BottomAppBar(
+        height: height * 0.6, // Updated height
         color: Colors.transparent,
-        child: SizedBox(
-          height: height * 0.5,
-          child: Container(
-            decoration: BoxDecoration(
-              color: context.read<ThemeBloc>().isLightTheme
-                  ? Colors.white
-                  : DarkThemeColor.primaryLight,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: context.read<ThemeBloc>().isLightTheme
+                    ? Colors.white
+                    : DarkThemeColor.primaryLight,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        RatingBar.builder(
-                          itemPadding: EdgeInsets.zero,
-                          itemSize: 20,
-                          initialRating: food.score,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          glow: false,
-                          ignoreGestures: true,
-                          itemBuilder: (context, _) => const FaIcon(
-                            FontAwesomeIcons.solidStar,
-                            color: LightThemeColor.yellow,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          RatingBar.builder(
+                            itemPadding: EdgeInsets.zero,
+                            itemSize: 20,
+                            initialRating: food.score,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            glow: false,
+                            ignoreGestures: true,
+                            itemBuilder: (context, _) => const FaIcon(
+                              FontAwesomeIcons.solidStar,
+                              color: LightThemeColor.yellow,
+                            ),
+                            onRatingUpdate: (_) {},
                           ),
-                          onRatingUpdate: (_) {},
-                        ),
-                        const SizedBox(width: 15),
-                        Text(
-                          food.score.toString(),
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        const SizedBox(width: 5),
-                        Text(
-                          "(${food.voter})",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        )
-                      ],
-                    ).fadeAnimation(0.4),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "\$${food.price}",
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge
-                              ?.copyWith(color: LightThemeColor.accent),
-                        ),
-                        BlocBuilder<FoodBloc, FoodState>(
-                          builder: (context, state) {
-                            return CounterButton(
-                              onIncrementSelected: () => context
-                                  .read<FoodBloc>()
-                                  .add(IncreaseQuantityEvent(food)),
-                              onDecrementSelected: () => context
-                                  .read<FoodBloc>()
-                                  .add(DecreaseQuantityEvent(food)),
-                              label: Text(
-                                state.foodList[state.foodList.getIndex(food)]
-                                    .quantity
-                                    .toString(),
-                                style: Theme.of(context).textTheme.displayLarge,
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    ).fadeAnimation(0.6),
-                    const SizedBox(height: 15),
-                    Text(
-                      "Description",
-                      style: Theme.of(context).textTheme.displayMedium,
-                    ).fadeAnimation(0.8),
-                    const SizedBox(height: 15),
-                    Text(
-                      food.description,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ).fadeAnimation(0.8),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 45,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.1),
-                        child: ElevatedButton(
-                          onPressed: () => context
-                              .read<FoodBloc>()
-                              .add(AddToCartEvent(food)),
-                          child: const Text("Add to cart"),
-                        ),
-                      ),
-                    )
-                  ],
+                          const SizedBox(width: 15),
+                          Text(
+                            food.score.toString(),
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            "(${food.voter})",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          )
+                        ],
+                      ).fadeAnimation(0.4),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$${food.price}",
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(color: LightThemeColor.accent),
+                          ),
+                          BlocBuilder<FoodBloc, FoodState>(
+                            builder: (context, state) {
+                              return CounterButton(
+                                onIncrementSelected: () => context
+                                    .read<FoodBloc>()
+                                    .add(IncreaseQuantityEvent(food)),
+                                onDecrementSelected: () => context
+                                    .read<FoodBloc>()
+                                    .add(DecreaseQuantityEvent(food)),
+                                label: Text(
+                                  state.foodList[state.foodList.getIndex(food)]
+                                      .quantity
+                                      .toString(),
+                                  style:
+                                      Theme.of(context).textTheme.displayLarge,
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      ).fadeAnimation(0.6),
+                      const SizedBox(height: 15),
+                      Text(
+                        "Description",
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ).fadeAnimation(0.8),
+                      const SizedBox(height: 15),
+                      Text(
+                        food.description,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ).fadeAnimation(0.8),
+                      const SizedBox(height: 70), // Updated spacing
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+            Positioned(
+              bottom: 10,
+              left: width * 0.1,
+              right: width * 0.1,
+              child: SizedBox(
+                width: width,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () =>
+                      context.read<FoodBloc>().add(AddToCartEvent(food)),
+                  child: Text(
+                    "Add to cart",
+                    style: TextStyle(
+                      color: context.read<ThemeBloc>().isLightTheme
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       body: ScaleAnimation(
-        child: Center(child: Image.asset(food.image, scale: 2)),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0), // Added padding
+            child: Image.asset(food.image, scale: 2),
+          ),
+        ),
       ),
     );
   }
