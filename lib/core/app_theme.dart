b9b3dda0ff2ac3ab51ced4312bf1551ad5d4bd07
SINASYS <sinasystem47@gmail.com>
2024-09-05 02:53:13 +0430

    Replace MaterialStateProperty with WidgetStateProperty

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -9,6 +9,7 @@ class AppThemes {
 
   static final appThemeData = {
     AppTheme.lightTheme: ThemeData(
+      useMaterial3: false,
       scaffoldBackgroundColor: LightThemeColor.primaryLight,
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
         backgroundColor: LightThemeColor.accent,
@@ -22,7 +23,7 @@ class AppThemes {
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
+          backgroundColor: WidgetStateProperty.all<Color>(
             LightThemeColor.accent,
           ),
         ),
@@ -54,6 +55,7 @@ class AppThemes {
       bottomAppBarTheme: const BottomAppBarTheme(color: Colors.white),
     ),
     AppTheme.darkTheme: ThemeData(
+      useMaterial3: false,
       canvasColor: DarkThemeColor.primaryDark,
       scaffoldBackgroundColor: DarkThemeColor.primaryDark,
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
@@ -69,7 +71,7 @@ class AppThemes {
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
+          backgroundColor: WidgetStateProperty.all<Color>(
             LightThemeColor.accent,
           ),
         ),
