0a8552ef7792aa4f9c9e611fb1942cee663cfa6a
SINASYS <sinasystem47@gmail.com>
2024-10-04 00:46:45 +0330

    Add extension for build context

--- lib/core/app_extension.dart
+++ lib/core/app_extension.dart
@@ -9,9 +9,8 @@ extension IterableWithIndex<T> on Iterable<T> {
   Iterable<T> distinctBy(Object Function(T e) getCompareValue) {
     var result = <T>[];
     forEach(
-          (element) {
-        if (!result
-            .any((x) => getCompareValue(x) == getCompareValue(element))) {
+      (element) {
+        if (!result.any((x) => getCompareValue(x) == getCompareValue(element))) {
           result.add(element);
         }
       },
@@ -28,4 +27,20 @@ extension WidgetExtension on Widget {
   Widget fadeAnimation(double delay) {
     return FadeAnimation(delay: delay, child: this);
   }
+}
+
+extension ContextExtensions on BuildContext {
+  TextStyle get headlineSmall => Theme.of(this).textTheme.headlineSmall!;
+
+  TextStyle get headlineMedium => Theme.of(this).textTheme.headlineMedium!;
+
+  TextStyle get displayMedium => Theme.of(this).textTheme.displayMedium!;
+
+  TextStyle get displaySmall => Theme.of(this).textTheme.displaySmall!;
+
+  TextStyle get displayLarge => Theme.of(this).textTheme.displayLarge!;
+
+  TextStyle get bodyLarge => Theme.of(this).textTheme.bodyLarge!;
+
+  TextStyle get titleMedium => Theme.of(this).textTheme.titleMedium!;
 }
